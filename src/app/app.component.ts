3968effd7dc651371538eea500d8fd323178efb2
Adeal46 <adililema3@gmail.com>
2018-12-02 14:20:18 +0300

    added folder structure used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
-@Component({
+import {Quote} from './quote'
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Quotes';
-}
+  quotes = [
+    new Quote("Hard Times","Sam Holland","When hard times come,the tough get going",0,0,0),
+    new Quote("Enjoyment","Ezio Hammarn","Joie de vivre",2,1,0),
+  ]
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
-@NgModule({
+import {FormsModule} from '@angular/forms';
+ import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { TimePipe } from './time.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+ @NgModule({
   declarations: [
     AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    TimePipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
-export class AppModule { }
+ })
+ export class AppModule { }
+ 
