455fccc3f07c8700366276e87a836566393535ad
Adeal46 <adililema3@gmail.com>
2018-12-02 14:40:32 +0300

    added page styling

---
+++ src/app/highest.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighest]'
+})
+export class HighestDirective {
+  constructor(private elem: ElementRef) { }
+
+  //   @HostListener("click") onClicks(){
+  //   this.textDeco("yellow")
+  // }
+  // @HostListener("dblclick") onDoubleClicks(){
+  //   this.textDeco("red")
+  // }
+  // $scope.max = Math.max.apply(Math,$scope.data.map(function(quote){
+  //   return quote.like;
+  // }));
+  // Math.max(quotes.like){
+  //   this.textDeco("line-through")
+  // }
+  // 
+  //   @HostListener() highestLike(){
+  //     let max:number;
+  //     max= 5;
+  //     if(this.quote.like>max){
+  //       this.textDeco("yellow")
+  //     }
+  // }
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
+}
