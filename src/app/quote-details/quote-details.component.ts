bd282dab43d2a2bcac471a20879fe8d0e45f4b23
Adeal <adililema3@gmail.com>
2019-01-17 08:50:16 +0300

    quote posting succesfully

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Quote } from '../quote';
+import { Component, OnInit,Input, Output, EventEmitter} from '@angular/core';
+import {Quote} from '../quote';
+
+
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-  @Input() quote: Quote;
+  @Input() quote:Quote;
   @Output() isDelete = new EventEmitter<boolean>();
+
   @Output() isRating = new EventEmitter<boolean>();
-  increaseRating(rating: boolean) {
+
+  increaseRating(rating:boolean) {
     this.isRating.emit(rating);
   }
-  deleteQuote(complete: boolean) {
+
+  deleteQuote(complete:boolean){
     this.isDelete.emit(complete);
   }
+
   constructor() { }
+
   ngOnInit() {
   }
+
 }
